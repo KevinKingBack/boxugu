5a5a9f0fa354f1c93fc4cd8a3f177979bf4cae29
kevin968 <626869329@qq.com>
2017-03-20 23:27:32 +0800

    查看和提交

--- public/js/config.js
+++ public/js/config.js
@@ -5,6 +5,13 @@ require.config({
         jquery: 'jquery/jquery.min',
         cookic: 'jquery-cookie/jquery.cookie',
         echarts:'echarts/echarts.min',
-        template:'artTemplate/template'
+        template:'artTemplate/template',
+        bootstrap:'bootstrap/js/bootstrap'
+    },
+    shim:{
+        //暂时先不加
+        bootstrap:{
+            deps:["jquery"]
+        }
     }
 });
---
+++ public/js/teacher-list.js
@@ -0,0 +1,64 @@
+/**
+ * 教师列表功能 
+ */
+define(["jquery", "template", "bootstrap"], function ($, template) {
+    $.ajax({
+        type: 'get',
+        url: '/api/teacher',
+        data: null,
+        dataType: 'json',
+        success: function (data) {
+            var html = template("teacherTpl", { list: data.result });
+            $("#teacherlist").html(html);
+            //查看讲师功能  
+            $('.teacherBtns').find('a:eq(0)').click(function () {
+                var tc_id = $(this).closest('td').attr('data-tcid');
+                $.ajax({
+                    type: 'get',
+                    url: '/api/teacher/view',
+                    data: { tc_id: tc_id },
+                    dataType: 'json',
+                    success: function (data) {
+                        if(data.code == 200){
+                            data.result.tc_hometown=data.result.tc_hometown.replace(/\|/g,' ');
+                            // data.result.tc_hometown=data.result.tc_hometown.split('|').join(' ');
+                            var html = template("teacherInfoModal", data.result);
+                            $("#teacher_Info").html(html);
+                            $("#teacherModal").modal();
+                        }
+
+                    }
+                });
+                return false;
+            });
+             //启用和注销功能
+            $('.teacherBtns').find('a:eq(2)').click(function () {
+                var that=this;
+                var td=$(this).closest('td');
+                var tc_id = td.attr('data-tcid');
+                var tc_status=td.attr('data-status');
+                $.ajax({
+                    type: 'post',
+                    url: '/api/teacher/handle',
+                    data: { tc_id: tc_id,tc_status:tc_status},
+                    dataType: 'json',
+                    success: function (data) {
+                        if(data.code == 200){
+                           if(data.result.tc_status ==0){
+                                $(that).text('注销');
+                           }else{
+                                $(that).text('启用');
+                           }
+                           //修改浏览器端的状态
+                           td.attr('data-status',data.result.tc_status);
+                        }
+                    }
+                });
+                return false;
+            });
+        }
+
+    });
+
+
+});
